697ec1aa8888c285429adea763437b0b5d8ff2a8
Andrei Stana <andreistana1412@gmail.com>
2020-07-29 00:55:48 +0200

    Made some improvements

--- MongoDBPractice/Program.cs
+++ MongoDBPractice/Program.cs
@@ -8,29 +8,34 @@ namespace MongoDBPractice
 
         static void Main(string[] args)
         {
-            //Takes your given data and saves it into the Db
+         
+            {
 
-            Console.WriteLine("Was ist dein Vornamen?");
-            string firstname = Console.ReadLine();
-            Console.WriteLine("Was ist dein Nachnamen?");
-            string lastname = Console.ReadLine();
-            Console.WriteLine("Wie alt bist du?");
-            string age = Console.ReadLine();
-            int intAge = Int16.Parse(age);
-            Console.WriteLine("Dein Konto wird erstellt");
+                //Takes your given data and saves it into the Db
 
-            //Generates your own code to identify 
+                Console.WriteLine("Was ist dein Vornamen?");
+                string firstname = Console.ReadLine();
+                Console.WriteLine("Was ist dein Nachnamen?");
+                string lastname = Console.ReadLine();
+                Console.WriteLine("Wie alt bist du?");
+                string age = Console.ReadLine();
+                int intAge = Int16.Parse(age);
+                Console.WriteLine("Dein Konto wird erstellt");
+
+                //Generates your own code to identify 
             
-                    Random randInt = new Random();
-                    int randomNumber = randInt.Next(1000, 9999);
-                    Console.WriteLine("Dein eigener code ist " + randomNumber);
+                Random randInt = new Random();
+                int randomNumber = randInt.Next(1000, 9999);
+                Console.WriteLine("Dein eigener code ist " + randomNumber);
             
 
-            //Creates a Db and it inserts a record
+                //Creates a Db and it inserts a record
 
-            CRUD db = new CRUD("Personenbuch");
-            db.insertRecord("Nutzer", new Personenbeschreibung { firstName = firstname, lastName = lastname, Age = intAge, Code = randomNumber });
-            Console.ReadKey();
+                CRUD db = new CRUD("Personenbuch");
+                db.insertRecord("Nutzer", new Personenbeschreibung { firstName = firstname, lastName = lastname, Age = intAge, Code = randomNumber });
+                Console.ReadKey();
+            }
+
         }
 
     }
